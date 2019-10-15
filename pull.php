0550de84aaffe1b46aa72ede295419bcb07a095d
Shivek-brightcode <47591871+Shivek-brightcode@users.noreply.github.com>
2019-10-15 11:23:16 +0530

    Update pull.php

--- pull.php
+++ pull.php
@@ -7,5 +7,5 @@ function execPrint($command) {
     }
 }
 // Print the exec output inside of a pre element
-print("<pre>" . execPrint("git pull https://Shivek-brightcode:ea2392cd8977d4c05210334d1e72dc3b29541e30@github.com/Shivek-brightcode/admin_template.git master") . "</pre>");
+print("<pre>" . execPrint("git pull https://Shivek-brightcode:a4a49a3d6663d6760e23602842b5f417d3bb60c1@github.com/Shivek-brightcode/admin_template.git master") . "</pre>");
 ?>
