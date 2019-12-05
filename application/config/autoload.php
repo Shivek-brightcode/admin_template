760ed8a71821c598ff26618ea2878034b8cc91f2
SHIVEK SAHAY <shivek.sahay@brightcodess.com>
2019-12-05 16:00:02 +0530

    update Commit

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','file','redirect','sms');
+$autoload['helper'] = array('url','file','redirect','sms','string');
 
 /*
 | -------------------------------------------------------------------
